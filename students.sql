976c507a102360e906ecb3b99614601a4c9b4ec0
NaiaAlmoudareys <151873469+NaiaAlmoudareys@users.noreply.github.com>
2023-11-23 14:04:57 -0500

    Create students.sql

---
+++ students.sql
@@ -0,0 +1,13 @@
+create table students
+	(student_id		SERIAL PRIMARY KEY  ,
+	 first_name		varchar(255) NOT NULL,
+	 last_name		varchar(255) NOT NULL,
+	 email  		varchar(255)NOT NULL UNIQUE, 
+	 enrollment_date	DATE
+	);
+	
+	
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
+('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
+('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');
